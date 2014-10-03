b55b90fd28f666c88388c741351b1caf7cdcf8d3
womei <womeijer@gmail.com>
2014-10-03 10:35:04 +0200

    Update getAng.py
    added command line interface so that you can select the limbs you want the angles from and if you want data from the head.

--- getAng.py
+++ getAng.py
@@ -1,23 +1,51 @@
-    """
-    this prints the angles (first from the left arm, then the right one)
-
-    """
+#!/usr/bin/env python
+"""
+this  grabs the angles of the arms and head (given the correct arguments)
+It then prints them to the command line
+"""
 import argparse
 import sys
+import math
 import rospy
+import  baxter_interface
+from std_msgs.msg import String
+rospy.init_node('angles', anonymous=True)
 
-from baxter_interface import (
-    DigitalIO,
-)
+def getInfo(limb, head):
+	if limb == 'left' or limb == 'both':
+		Llimb = baxter_interface.Limb('left')
+		Langles = Llimb.joint_angles()
+		print Langles
+
+	if limb == 'right' or limb == 'both':
+		Rlimb = baxter_interface.Limb('right')
+		Rangles = Rlimb.joint_angles()
+		print Rangles
+
+	if head:
+		head = baxter_interface.Head()
+		print 'Head angle: ' head.pan()
+		if head.nodding():
+			print 'Baxter is nodding :D'
+		else:
+			print 'not nodding :('
 
 
-Llimb = baxter_interface.Limb('left')
-Langles = Llimb.joint_angles()
+def main():
+	arg_fmt = argparse.RawDescriptionHelpFormatter
+	parser = argparse.ArgumentParser(formatter_class=arg_fmt, description=main.__doc__)
 
-Rlimb = baxter_interface.Limb('right')
-Rangles = Rlimb.joint_angles()
+	parser.add_argument('-a', '--arm', dest='arm', default='both',
+		choices=['both', 'left', 'right'],
+		help='arm to get data from (default: both)')
+	parser.add_argument('-h', '--head', dest='head',
+		action='store_true',
+		help='also show head data')
 
-print "showing left angles"
-print Langles
-print "showing right angles"
-print Rangles
+
+	args = parser.parse_args(rospy.myargv()[1:])
+
+	getInfo(args.arm,args.head)
+
+if __name__ == '__main__':
+    main()
